79210138bf89671f4e92f60c9308d2ce427ec3f1
Gabriele201 <gabriele.535239201@stu.untar.ac.id>
2024-09-24 20:06:43 +0700

    tampilan home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -38,43 +38,43 @@ class _HomeScreenState extends State<HomeScreen> {
         items: const [
           BottomNavigationBarItem(
             icon: Padding(
-              padding: EdgeInsets.all(8.0), 
+              padding: EdgeInsets.all(8.0),
               child: Icon(CupertinoIcons.home),
             ),
             label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: Padding(
-              padding: EdgeInsets.all(8.0), 
+              padding: EdgeInsets.all(8.0),
               child: Icon(CupertinoIcons.money_dollar),
             ),
             label: 'Transactions',
           ),
           BottomNavigationBarItem(
             icon: Padding(
-              padding: EdgeInsets.all(8.0), 
+              padding: EdgeInsets.all(8.0),
               child: Icon(CupertinoIcons.qrcode),
             ),
             label: 'QRIS',
           ),
           BottomNavigationBarItem(
             icon: Padding(
-              padding: EdgeInsets.all(8.0), 
+              padding: EdgeInsets.all(8.0),
               child: Icon(CupertinoIcons.bell),
             ),
             label: 'Reminders',
           ),
           BottomNavigationBarItem(
             icon: Padding(
-              padding: EdgeInsets.all(8.0), 
+              padding: EdgeInsets.all(8.0),
               child: Icon(CupertinoIcons.profile_circled),
             ),
             label: 'Profile',
           ),
         ],
-        backgroundColor: const Color(0xFF301F17), 
-        activeColor: Colors.white, 
-        inactiveColor: Colors.grey, 
+        backgroundColor: const Color(0xFF301F17),
+        activeColor: Colors.white,
+        inactiveColor: Colors.grey,
       ),
       tabBuilder: (context, index) {
         return CupertinoTabView(
@@ -85,8 +85,15 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
   const HomeContent({super.key});
+
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  bool _isBalanceVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -108,14 +115,157 @@ class HomeContent extends StatelessWidget {
               ),
             ),
           ),
-          const Center(
-            child: Text(
-              'Home Screen',
-              style: TextStyle(
-                color: Colors.white, 
-                fontSize: 24, 
+          Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF301f17),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/sarang_wallet2.png',
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                  const Positioned(
+                    bottom: 10,
+                    left: 25,
+                    child: Text(
+                      'Hello, User',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
+              const SizedBox(height: 20),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    'assets/card.png',
+                    height: 225,
+                  ),
+                  const Positioned(
+                    top: 62,
+                    left: 87,
+                    child: Text(
+                      'Your account balance',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.brown,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 115,
+                    left: 105,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            _isBalanceVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.brown,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isBalanceVisible = !_isBalanceVisible;
+                            });
+                          },
+                        ),
+                        Text(
+                          _isBalanceVisible ? 'Rp. 123.456.789' : 'Rp. **********',
+                          style: const TextStyle(
+                            fontFamily: 'Poppins',
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30), 
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Promo & Discount',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFF3E9DC), 
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 200,
+                child: ListView(
+                  children: [
+                    Container(
+                      height: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFD7CCC8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFD7CCC8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFD7CCC8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFD7CCC8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFD7CCC8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
