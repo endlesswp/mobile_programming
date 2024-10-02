dcef2b9f6f22f6902699e36e3dde9a15f834affc
Gabriele201 <gabriele.535239201@stu.untar.ac.id>
2024-10-02 20:19:56 +0700

    tambahan reminder screen

--- lib/screens/reminder_screen.dart
+++ lib/screens/reminder_screen.dart
@@ -37,32 +37,50 @@ class _ReminderScreenState extends State<ReminderScreen> {
     dateController.clear();
   }
 
+  void _deleteReminder(int index) {
+    setState(() {
+      reminders.removeAt(index);
+    });
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      setState(() {
+        dateController.text = "${picked.toLocal()}".split(' ')[0];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF4E342E),
-                    Color(0xFF8D6E63),
-                    Color(0xFF4E342E),
-                  ],
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                ),
-              ),
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF4E342E),
+              Color(0xFF8D6E63),
+              Color(0xFF4E342E),
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
+        ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
                   'Reminder',
                   textAlign: TextAlign.center,
                   style: GoogleFonts.dancingScript(
@@ -71,104 +89,129 @@ class _ReminderScreenState extends State<ReminderScreen> {
                     color: Colors.white,
                   ),
                 ),
-                const SizedBox(height: 10),
-                const Text(
-                  'For Sarang Wallet Friends',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: 'Poppins',
-                    color: Colors.white,
-                  ),
+              ),
+              const Text(
+                'For Sarang Wallet Friends',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
                 ),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Set Your Reminder',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: Column(
-                        children: [
-                          const Text(
-                            'Set Your Reminder',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Title',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: amountController,
+                      decoration: const InputDecoration(
+                        labelText: 'Amount',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () => _selectDate(context),
+                      child: AbsorbPointer(
+                        child: TextField(
+                          controller: dateController,
+                          decoration: const InputDecoration(
+                            labelText: 'Date',
+                            border: OutlineInputBorder(),
                           ),
-                          const SizedBox(height: 20),
-                          TextField(
-                            controller: titleController,
-                            decoration: const InputDecoration(
-                              labelText: 'Title',
-                              border: OutlineInputBorder(),
-                            ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close, color: Colors.red),
+                          onPressed: _deleteInputs,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.check, color: Colors.green),
+                          onPressed: _saveReminder,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Reminders List:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              const SizedBox(height: 5),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: reminders.isNotEmpty 
+                  ? ListView.builder(
+                      itemCount: reminders.length,
+                      itemBuilder: (context, index) {
+                        final reminder = reminders[index];
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF8D6E63),
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          const SizedBox(height: 10),
-                          TextField(
-                            controller: amountController,
-                            decoration: const InputDecoration(
-                              labelText: 'Amount',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          TextField(
-                            controller: dateController,
-                            decoration: const InputDecoration(
-                              labelText: 'Date',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
+                          child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              IconButton(
-                                icon: const Icon(Icons.close, color: Colors.red),
-                                onPressed: _deleteInputs,
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Title: ${reminder['title']}', style: const TextStyle(color: Colors.white)),
+                                    Text('Amount: ${reminder['amount']}', style: const TextStyle(color: Colors.white)),
+                                    Text('Date: ${reminder['date']}', style: const TextStyle(color: Colors.white)),
+                                  ],
+                                ),
                               ),
                               IconButton(
-                                icon: const Icon(Icons.check, color: Colors.green),
-                                onPressed: _saveReminder,
+                                icon: const Icon(Icons.delete, color: Colors.red),
+                                onPressed: () => _deleteReminder(index),
                               ),
                             ],
                           ),
-                          const SizedBox(height: 20),
-                          const Text(
-                            'Reminders List:',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 10),
-                          ...reminders.map((reminder) {
-                            return Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                color: const Color(0xFF8D6E63),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('Title: ${reminder['title']}'),
-                                  Text('Amount: ${reminder['amount']}'),
-                                  Text('Date: ${reminder['date']}'),
-                                ],
-                              ),
-                            );
-                          }).toList(),
-                        ],
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text(
+                        'No reminders yet!',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
